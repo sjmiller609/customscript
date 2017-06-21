2bf20b0a05d378d77ca4b7f8f1ee96ce507b6c40
Steven Miller <Steven Miller>
2017-06-20 22:57:54 -0400

    testing

--- customscript.py
+++ customscript.py
@@ -1,11 +1,17 @@
 
-import os
 
+
+
+with open("iwashere.txt","w") as f:
+    f.write("custom script hello world")
+
+with open("C:\\Users\\shoetest\\Desktop\\iwashere2.txt","w") as f:
+    f.write("custom script hello world")
+
+
+import os
 try:
     os.system('Start "SoleSlayer" "C:\\Program Files (x86)\\SoleSlayer"')
 except:
     pass
 
-
-with open("~/Desktop/testfile.txt","w") as f:
-    f.write("custom script hello world")
