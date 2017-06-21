25d396d49c8a1b55665f628822307dbd02157886
Steven Miller <Steven Miller>
2017-06-20 21:10:53 -0400

    hello world

---
+++ customscript.py
@@ -0,0 +1,11 @@
+
+import os
+
+try:
+    os.system('Start "SoleSlayer" "C:\\Program Files (x86)\\SoleSlayer"')
+except:
+    pass
+
+
+with open("~/Desktop/testfile.txt","w") as f:
+    f.write("custom script hello world")
