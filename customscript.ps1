7d1c8925d0c2ffeff4163e19279a816b91654210
Steven Miller <Steven Miller>
2017-06-20 23:32:20 -0400

    debugging

--- customscript.ps1
+++ customscript.ps1
@@ -2,4 +2,4 @@ New-Item C:\Users\shoetest\test -type file
 "testing"
 git clone https://github.com/sjmiller609/customscript.git
 "testing2"
-python .\customscript\customscript.py
+C:\Users\shoetest\AppData\Local\Programs\Python\Python36-32\python .\customscript\customscript.py
