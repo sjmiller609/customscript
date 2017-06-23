4421e1cc24c4f74bc7e60a6d202b7e599e1f06ec
Steven Miller <Steven Miller>
2017-06-22 23:36:38 -0400

    fix

--- customscript.ps1
+++ customscript.ps1
@@ -1,5 +1,5 @@
-New-Item C:\Users\shoetest\test -type file
+New-Item C:\Users\shoetemplate\test -type file
 "testing"
 git clone https://github.com/sjmiller609/customscript.git
 "testing2"
-C:\Users\shoetest\AppData\Local\Programs\Python\Python36-32\python .\customscript\customscript.py
+C:\Users\shoetemplate\AppData\Local\Programs\Python\Python36-32\python .\customscript\customscript.py
